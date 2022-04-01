e0c0ffe912765c572a59014b55032423afaa9f55
yajiwa <839790708@qq.com>
2022-04-01 14:04:34 +0800

    fit nonebot.zhenxun.beta2

---
+++ nonebot_plugin_petpet/models.py
@@ -0,0 +1,28 @@
+from io import BytesIO
+from PIL import Image
+from PIL.Image import Image as IMG
+from dataclasses import dataclass
+from typing import List, Tuple, Union, Protocol
+
+
+class UserInfo:
+    def __init__(self, qq: str = "", group: str = "", img_url: str = ""):
+        self.qq: str = qq
+        self.group: str = group
+        self.name: str = ""
+        self.gender: str = ""  # male 或 female 或 unknown
+        self.img_url: str = img_url
+        self.img: IMG = Image.new("RGBA", (640, 640))
+
+
+class Func(Protocol):
+    async def __call__(self, users: List[UserInfo], **kwargs) -> Union[str, BytesIO]:
+        ...
+
+
+@dataclass
+class Command:
+    keywords: Tuple[str, ...]
+    func: Func
+    convert: bool = True
+    arg_num: int = 0
