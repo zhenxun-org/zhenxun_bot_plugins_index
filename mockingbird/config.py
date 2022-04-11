fac61f5d44ba29f858ff5cd98789b47f130cb999
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2022-04-11 15:22:05 +0800

    Update config.py

--- mockingbird/config.py
+++ mockingbird/config.py
@@ -4,4 +4,4 @@ from configs.config import Config as TConfig
 class Config(BaseModel, extra=Extra.ignore):
     tencent_secret_id: str = TConfig.get_config("mockingbird","TENCENT_SECRET_ID")
     tencent_secret_key: str = TConfig.get_config("mockingbird","TENCENT_SECRET_KEY")
-    tts_project_id: str = ""
+    tts_project_id: str = "4587666"
