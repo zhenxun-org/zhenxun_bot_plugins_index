874d3618212170d4efd8e4242d07564f2ec534ae
yajiwa <839790708@qq.com>
2022-05-23 21:01:20 +0800

    updat random_cat_gif

--- random_cat_gif/__init__.py
+++ random_cat_gif/__init__.py
@@ -2,7 +2,7 @@ from nonebot import on_command
 from nonebot.adapters.onebot.v11.bot import Bot
 from nonebot.adapters.onebot.v11 import MessageSegment
 from nonebot.adapters.onebot.v11.event import Event
-import requests
+from utils.http_utils import AsyncHttpx
 
 __zx_plugin_name__ = "来个猫猫"
 __plugin_usage__ = """
@@ -12,7 +12,8 @@ usage：
 __plugin_des__ = "来个猫猫"
 __plugin_type__ = ("一些工具",)
 __plugin_cmd__ = ["来个猫猫"]
-
+__plugin_version__ = 0.1
+__plugin_author__ = "yajiwa & Copaan"
 __plugin_settings__ = {
     "level": 5,
     "default_status": True,
@@ -20,10 +21,13 @@ __plugin_settings__ = {
     "cmd": ["来个猫猫"],
 }
 
+miao = on_command("来个猫猫", block=True, priority=5)
 
-miao=on_command("来个猫猫")
+
 @miao.handle()
-async def hf(bot:Bot,ev:Event):
-    import requests
-    img=requests.get("http://edgecats.net/")
-    await bot.send(event=ev,message=MessageSegment.image(img.content))
+async def hf(bot: Bot, ev: Event):
+    try:
+        img = await AsyncHttpx().get("http://edgecats.net/")
+    except:
+        return await bot.send(event=ev, message="获取猫猫图片超时")
+    await bot.send(event=ev, message=MessageSegment.image(img.content))
