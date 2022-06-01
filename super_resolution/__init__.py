3f3ab7cbd383e9cb4969216f21c6868008c75c98
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2022-06-02 02:12:21 +0800

    Update SuperReslution v0.2

--- super_resolution/__init__.py
+++ super_resolution/__init__.py
@@ -1,8 +1,11 @@
 import asyncio
 import time
+import io
+import imageio
 from asyncio import Semaphore
 from pathlib import Path
 
+import httpx
 import numpy as np
 from configs.path_config import TEMP_PATH
 from nonebot import on_command
@@ -10,8 +13,8 @@ from nonebot.adapters.onebot.v11 import Bot, Message, MessageEvent
 from nonebot.params import Arg, Depends
 from nonebot.typing import T_State
 from PIL import Image as IMG
+from PIL import ImageSequence
 from utils.http_utils import AsyncHttpx
-from utils.image_utils import pic2b64
 from utils.message_builder import image as image_builder
 from utils.utils import get_message_img
 
@@ -35,7 +38,7 @@ __plugin_cmd__ = [
     "超分 [image]",
 ]
 __plugin_type__ = ("一些工具",)
-__plugin_version__ = 0.1
+__plugin_version__ = 0.2
 __plugin_author__ = "AkashiCoin"
 __plugin_settings__ = {
     "level": 5,
@@ -60,7 +63,7 @@ upsampler = (
             num_grow_ch=32,
             scale=4,
         ),
-        tile=0,
+        tile=100,
         tile_pad=10,
         pre_pad=0,
         half=False,
@@ -69,20 +72,23 @@ upsampler = (
     else None
 )
 
-max_size = 691200
+max_size = 2073600
 mutex = Semaphore(1)
 processing = False
+use_api = False
+api_url = ""
 
 superResolution = on_command("超分", priority=5, block=True)
 
+
 def parse_image(key: str):
-    async def _key_parser(
-        state: T_State, img: Message = Arg(key)
-    ):
+    async def _key_parser(state: T_State, img: Message = Arg(key)):
         if not get_message_img(img):
             await superResolution.finish("格式错误，超分已取消...")
         state[key] = img
+
     return _key_parser
+
 
 @superResolution.handle()
 async def _(bot: Bot, event: MessageEvent, state: T_State):
@@ -102,6 +108,7 @@ async def _(
     img: Message = Arg("img"),
 ):
     global processing
+
     img_url = get_message_img(img)[0]
     resize = False
     if processing:
@@ -110,15 +117,17 @@ async def _(
     processing = True
     mutex.release()
     await superResolution.send("开始处理图片...")
+    super_file: Path = TEMP_PATH / (f"{event.user_id}_super.png")
     if await AsyncHttpx.download_file(
-            img_url, TEMP_PATH / f"{event.user_id}_super.png"
-        ):
-        image = IMG.open(TEMP_PATH / f"{event.user_id}_super.png")
+        img_url, super_file
+    ):
+        image = IMG.open(super_file)
     else:
         await mutex.acquire()
-        processing=False
+        processing = False
         mutex.release()
         await superResolution.finish("图片下载失败...")
+    is_gif = getattr(image, "is_animated", False)
     start = time.time()
     image_size = image.size[0] * image.size[1]
     if image_size > max_size:
@@ -138,14 +147,57 @@ async def _(
                 round(image.size[1] * magnification),
             )
         )
-    image_array: np.ndarray = image.__array__()
-    loop = asyncio.get_event_loop()
-    output, _ = await loop.run_in_executor(None, upsampler.enhance, image_array, 2)
+    result = io.BytesIO()
+    if use_api:
+        try:
+            async with httpx.AsyncClient() as client:
+                image.close()
+                with open(str(super_file), "rb") as f:
+                    img = f.read()
+                r = await client.post(
+                    api_url,
+                    files={"file": img},
+                    timeout=180
+                )
+                if r.status_code == 200:
+                    result = io.BytesIO(r.content)
+                    if "json" in r.headers.get("Content-Type"):
+                        await mutex.acquire()
+                        processing = False
+                        mutex.release()
+                        r = r.json()
+                        superResolution.finish(r["message"])
+                else:
+                    await superResolution.finish("超分失败，请稍后再试...")
+        except Exception as e:
+            await mutex.acquire()
+            processing = False
+            mutex.release()
+            await superResolution.finish(f"超分失败，请稍后再试...\n{type(e)}: {str(e)}")
+    else:
+        outputs = []
+        loop = asyncio.get_event_loop()
+        if is_gif:
+            for i in ImageSequence.Iterator(image):
+                image_array: np.ndarray = i.__array__()
+                output, _ = await loop.run_in_executor(None, upsampler.enhance, image_array, 2)
+                outputs.append(output)
+        else:
+            image_array: np.ndarray = image.__array__()
+            output, _ = await loop.run_in_executor(None, upsampler.enhance, image_array, 2)
+        if is_gif:
+            imageio.mimsave(result, outputs[1:], format='gif', duration=image.info["duration"] / 1000)
+        else:
+            img = IMG.fromarray(output)
+            img.save(result, format='PNG')  # format: PNG / JPEG
     await mutex.acquire()
     processing = False
     mutex.release()
-    img = IMG.fromarray(output)
     end = time.time()
     use_time = round(end - start, 2)
-    await superResolution.send(Message(f"超分完成！处理用时：{use_time}s\n" + (f"由于像素过大，图片已进行缩放，结果可能不如原图片清晰" if resize else "")))
-    await superResolution.finish(image_builder(b64=pic2b64(img)))
+    await superResolution.finish(
+        Message(
+            f"超分完成！处理用时：{use_time}s"
+            + (f"\n由于像素过大，图片已进行缩放，结果可能不如原图片清晰" if resize else "")
+        ) + image_builder(result.getvalue())
+    )
